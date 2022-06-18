12f015dc0a9f45d897a31f53beae03ac4f07bc1b
four420twenty <93328270+four420twenty@users.noreply.github.com>
2022-06-18 13:21:14 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>User Login</title>
+</head>
+<body>
+
+<h1>Login</h1>
+
+<form action="login.php" method="post">
+    Username: <input type="text" name="username" /> <br />
+    Password: <input type="password" name="password" /> <br />
+    <input type="submit" value="Login" />
+</form>
+
+</body>
+</html>
